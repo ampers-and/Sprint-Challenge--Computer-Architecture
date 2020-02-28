a93f0c6fcff72153502a007b5d139481391ca92a
ampersand <hossainazifa@gmail.com>
2020-02-28 10:54:56 -0800

    cpu py  added

---
+++ cpu.py
@@ -0,0 +1,154 @@
+"""CPU functionality."""
+
+import sys
+
+class CPU:
+    """Main CPU class."""
+
+    def __init__(self):
+        """Construct a new CPU."""
+        self.reg = [0] * 8
+        self.ram = [0] * 256
+        self.pc = 0
+        self.running = True
+
+        # # Load Immediate - 130
+        # LDI = 0b10000010 
+        # # Print - 71
+        # PRN = 0b01000111 
+        # # Multiply - 162
+        # MUL = 0b10100010
+        # # Add - 160
+        # ADD = 0b10100000
+        # # Push - 69
+        # PUSH = 0b01000101
+        # # Pop - 70
+        # POP = 0b01000110
+        # # Call - 80
+        # CALL = 0b01010000
+        # # Return - 17
+        # RET = 0b00010001
+        # # Halt - 1
+        # HLT = 0b00000001
+
+        self.instructions = {
+            130: self.ldi,
+            71: self.prn,
+            1: self.hlt,
+            162: self.mul,
+            # 160: self.add,
+            # 69: self.push,
+            # 70: self.pop,
+            # 80: self.call,
+            # 17: self.ret,
+
+        }
+
+    def ram_read(self, MAR):
+      return self.ram[MAR]
+    
+    def ram_write(self, MDR, MAR):
+      self.ram[MAR] = MDR
+
+    def hlt(self):
+        self.running = False
+        self.pc += 1
+
+    def ldi(self, reg_a, reg_b):
+        self.reg[reg_a] = reg_b
+        self.pc += 3
+
+    def prn(self, reg_a, reg_b):
+        print(self.reg[reg_a])
+        self.pc += 2
+
+    def mul(self, reg_a, reg_b):
+        self.alu("MUL", reg_a, reg_b)
+        self.pc += 3
+
+
+    def load(self):
+        """Load a program into memory."""
+
+
+        address = 0
+
+        # For now, we've just hardcoded a program:
+
+        program = [
+            # # From print8.ls8
+            # 0b10000010, # LDI R0,8
+            # 0b00000000,
+            # 0b00001000,
+            # 0b01000111, # PRN R0
+            # 0b00000000,
+            # 0b00000001, # HLT
+        ]
+
+        file = sys.argv[1]
+
+        with open(file) as f:
+                for line in f:
+                    text = line.split('#')
+                    num = text[0].strip()
+                    if num != '':
+                        program.append(int(num, 2))
+
+
+
+        for instruction in program:
+            self.ram[address] = instruction
+            address += 1
+
+
+    def alu(self, op, reg_a, reg_b):
+        """ALU operations."""
+
+        if op == "ADD":
+            self.reg[reg_a] += self.reg[reg_b]
+        #elif op == "SUB": etc
+
+        #mul
+        if op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
+        else:
+            raise Exception("Unsupported ALU operation")
+
+    def trace(self):
+        """
+        Handy function to print out the CPU state. You might want to call this
+        from run() if you need help debugging.
+        """
+
+        print(f"TRACE: %02X | %02X %02X %02X |" % (
+            self.pc,
+            #self.fl,
+            #self.ie,
+            self.ram_read(self.pc),
+            self.ram_read(self.pc + 1),
+            self.ram_read(self.pc + 2)
+        ), end='')
+
+        for i in range(8):
+            print(" %02X" % self.reg[i], end='')
+
+        print()
+
+    def run(self):
+        """Run the CPU."""
+
+        while self.running:
+            ir = self.ram_read(self.pc)
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+            # print('ir', ir)
+            if ir in self.instructions:
+                if ir == 80 or ir == 17 or ir == 1:
+                    self.instructions[ir]()
+                else:
+                    self.instructions[ir](operand_a, operand_b)
+                    #self.pc move handled in specific instructions
+
+            else:
+                print(f"Unknown instruction at index {self.pc}")
+                sys.exit(1)
